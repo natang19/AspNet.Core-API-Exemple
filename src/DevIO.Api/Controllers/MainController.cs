766d15ce7f012c67f086e4e099925d949fd6b5ba
Natan Guedes <natan@financas360.com.br>
2020-05-11 23:31:55 -0300

    Add and start the FornecedorController, AutoMapper with your configuration and mappings, add dependecy injection resolver class.

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de erro
+
+        // validacao de model state
+
+        // validação da operação de negocios
+
+        //
+    }
+}
